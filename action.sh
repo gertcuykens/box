6b5f00a9d79c8d24dda8d47ae77773d61628174c
Gert Cuykens <gert.cuykens@gmail.com>
2022-11-04 17:25:30 +0100

    _

--- action.sh
+++ action.sh
@@ -5,3 +5,7 @@ set -eu
 printf '\033[33m Start a test action.\033[0m\n'
 
 echo "$INPUT_HOST"
+echo "msg=OK" >> $GITHUB_OUTPUT
+
+# git update-index --chmod=+x action.sh
+# git ls-files --stage action.sh
