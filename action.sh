6bcfcf430df4352c647cc7aa3c7640dab70f1e6f
Gert Cuykens <gert.cuykens@gmail.com>
2022-11-04 20:51:33 +0100

    _

--- action.sh
+++ action.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 set -eu
-printf '\033[33mStart a test action.\033[0m\n'
-echo "$INPUT_HOST"
-echo "msg=OK" >> $GITHUB_OUTPUT
+printf '\033[33m/root/action.sh\033[0m\n'
+echo "$INPUT_MSG"
+echo "msg=world" >> $GITHUB_OUTPUT
 
 # git update-index --chmod=+x action.sh
 # git ls-files --stage action.sh
