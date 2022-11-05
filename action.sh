10d381f917f4ce9228aa8bb365d338e7afc48e43
Gert Cuykens <gert.cuykens@gmail.com>
2022-11-05 03:28:30 +0100

    _

--- action.sh
+++ action.sh
@@ -6,3 +6,6 @@ echo "msg=world" >> $GITHUB_OUTPUT
 
 # git update-index --chmod=+x action.sh
 # git ls-files --stage action.sh
+
+# ssh-keygen -t ed25519 -C "...@..."
+# https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent?platform=linux
